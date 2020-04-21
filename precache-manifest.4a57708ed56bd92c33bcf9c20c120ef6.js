8b57dcccf5f8c52de08fa9379aa1ca3f5ae26e17
punisher49 <aliyev.hasanagha@gmail.com>
2020-04-21 11:36:26 +1000

    Updates

---
+++ precache-manifest.4a57708ed56bd92c33bcf9c20c120ef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b945f5e91297ff9a1a585015aee5681",
+    "url": "/competitor-review-frontend/index.html"
+  },
+  {
+    "revision": "031993abd89c8a171a54",
+    "url": "/competitor-review-frontend/static/css/2.05988dc8.chunk.css"
+  },
+  {
+    "revision": "e540b214c82fa8867064",
+    "url": "/competitor-review-frontend/static/css/main.bae14c16.chunk.css"
+  },
+  {
+    "revision": "031993abd89c8a171a54",
+    "url": "/competitor-review-frontend/static/js/2.43ca2921.chunk.js"
+  },
+  {
+    "revision": "43c90780224d05185bec687a556a8de7",
+    "url": "/competitor-review-frontend/static/js/2.43ca2921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e540b214c82fa8867064",
+    "url": "/competitor-review-frontend/static/js/main.3207a0f4.chunk.js"
+  },
+  {
+    "revision": "045579b1f95851e3abf9",
+    "url": "/competitor-review-frontend/static/js/runtime-main.c9c07725.js"
+  }
+]);
