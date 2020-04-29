9f88638b5161ab02381725c4f1d1a66debb1935b
punisher49 <aliyev.hasanagha@gmail.com>
2020-04-29 14:28:59 +1000

    Updates

---
+++ precache-manifest.d9a357670784d84ab1c9a33ce8cc0b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfceb2b4454a25069ad3d98bc1d33ec",
+    "url": "/competitor-review/index.html"
+  },
+  {
+    "revision": "ab9dd505a28226645dbd",
+    "url": "/competitor-review/static/css/2.05988dc8.chunk.css"
+  },
+  {
+    "revision": "236fe0db6c6ef39d782a",
+    "url": "/competitor-review/static/css/main.74b4a829.chunk.css"
+  },
+  {
+    "revision": "ab9dd505a28226645dbd",
+    "url": "/competitor-review/static/js/2.63420cf8.chunk.js"
+  },
+  {
+    "revision": "179b366070823dbb290ea38dd24e68eb",
+    "url": "/competitor-review/static/js/2.63420cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236fe0db6c6ef39d782a",
+    "url": "/competitor-review/static/js/main.45c7fa7f.chunk.js"
+  },
+  {
+    "revision": "1deb9431c91afdf0a154",
+    "url": "/competitor-review/static/js/runtime-main.45b0d56c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/competitor-review/precache-manifest.f72313221ce235aa8e729b1547057022.js"
+  "/competitor-review/precache-manifest.d9a357670784d84ab1c9a33ce8cc0b55.js"
 );
 
 self.addEventListener('message', (event) => {
