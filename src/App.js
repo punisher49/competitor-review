b3de88f55e6a8f56a97564aab1a78fc63b13794b
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-05 07:04:35 +1000

    new login

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
 	render(){
 		return (
 			<Provider store={store}>
-				<HashRouter basename="/">
+				<HashRouter>
 					<Navbar />
 					<Route exact  path="/" component={Home}/>
 					<Route path="/aeons" component={AeonsList} />
