9ffc82fbf827880135a7332b79fb2ffef2e8f025
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-05 12:04:11 +1000

    mobile version

--- src/store.js
+++ src/store.js
@@ -1,13 +1,27 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
+// import {createStore, applyMiddleware, compose} from 'redux';
+// import thunk from 'redux-thunk'
+// import rootReducer from './reducers'
+//
+// const initialState = {}
+// const middleware = [thunk];
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(applyMiddleware(...middleware) )
+// )
+//
+// export default store
+//
 
-const initialState = {}
+
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer  from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+const initialState = {};
 const middleware = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(applyMiddleware(...middleware) )
-)
+const applied = composeWithDevTools ( applyMiddleware(...middleware) ); //error!
+const store = createStore(rootReducer, initialState, applied);
 
-export default store
+
+export default store;
