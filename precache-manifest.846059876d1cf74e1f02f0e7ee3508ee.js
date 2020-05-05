1da9e3be05bfd2e302f9bba5b2b7d71bb9a235f8
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-05 11:25:17 +1000

    Updates

--- precache-manifest.846059876d1cf74e1f02f0e7ee3508ee.js
+++ precache-manifest.846059876d1cf74e1f02f0e7ee3508ee.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "635ec8cb758ae6f013d047c4a2bdfdbc",
+    "revision": "888ef315ba6b1a5288a70dbeee725f23",
     "url": "/competitor-review/index.html"
   },
   {
-    "revision": "a8fecc63929830194aa3",
+    "revision": "6903088d66df4142f0a9",
     "url": "/competitor-review/static/css/2.05988dc8.chunk.css"
   },
   {
-    "revision": "7bc03d54a142166f08ef",
+    "revision": "b97550015f796ea36c29",
     "url": "/competitor-review/static/css/main.78ea1aec.chunk.css"
   },
   {
-    "revision": "a8fecc63929830194aa3",
-    "url": "/competitor-review/static/js/2.992c4671.chunk.js"
+    "revision": "6903088d66df4142f0a9",
+    "url": "/competitor-review/static/js/2.5f1d9530.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/competitor-review/static/js/2.992c4671.chunk.js.LICENSE.txt"
+    "url": "/competitor-review/static/js/2.5f1d9530.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7bc03d54a142166f08ef",
-    "url": "/competitor-review/static/js/main.0687e2f8.chunk.js"
+    "revision": "b97550015f796ea36c29",
+    "url": "/competitor-review/static/js/main.e2a5b82e.chunk.js"
   },
   {
     "revision": "1deb9431c91afdf0a154",
