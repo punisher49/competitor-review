ae316c989196d87365cba172488c7666d7f3f4d9
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-09 13:56:29 +1000

    Updates

--- precache-manifest.087f89257b2da9f06a39efe39635db1d.js
+++ precache-manifest.087f89257b2da9f06a39efe39635db1d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11e876198dd529a5e0c45e3b74bc3570",
+    "revision": "9f0d7b5fbfb4a80caf29dbb55b76cf88",
     "url": "/competitor-review/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/competitor-review/static/css/2.05988dc8.chunk.css"
   },
   {
-    "revision": "41a20233aa223ca4af79",
-    "url": "/competitor-review/static/css/main.78ea1aec.chunk.css"
+    "revision": "bd6cbbb66207ba7c532d",
+    "url": "/competitor-review/static/css/main.fbca858f.chunk.css"
   },
   {
     "revision": "68fcffea6f1416208bc4",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/competitor-review/static/js/2.ed0cf93e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "41a20233aa223ca4af79",
-    "url": "/competitor-review/static/js/main.7ca029ab.chunk.js"
+    "revision": "bd6cbbb66207ba7c532d",
+    "url": "/competitor-review/static/js/main.200c45a3.chunk.js"
   },
   {
     "revision": "1deb9431c91afdf0a154",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/competitor-review/precache-manifest.844f2dbbcd23a57589485b742d90cde1.js"
+  "/competitor-review/precache-manifest.087f89257b2da9f06a39efe39635db1d.js"
 );
 
 self.addEventListener('message', (event) => {
