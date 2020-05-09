a2e27d2767319ca8ee1d1cb76a1469aa0c100697
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-09 19:05:49 +1000

    ready redux

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,67 +44,69 @@ class Navbar extends Component {
     const normalLinks = (
       <Fragment>
         <div className="dropdown" onClick={this.toggleOpen} id="dropdown">
-        <button
-          className="btn btn-warning dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-        >
-          Baby Food Markets
-        </button>
-        <div className={menuClass} aria-labelledby="dropdownMenuButton">
+          <button
+            className="btn btn-warning dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            >
+            Baby Food Markets
+          </button>
+          <div className={menuClass} aria-labelledby="dropdownMenuButton">
 
-        <Link to="/ranchos" className="dropdown-item">
-            Ranch Market
-          </Link>
+            <Link to="/ranchos" className="dropdown-item">
+              Ranch Market
+            </Link>
 
-        <Link to="/grands" className="dropdown-item">
-            Grand Lucky
-          </Link>
+            <Link to="/grands" className="dropdown-item">
+              Grand Lucky
+            </Link>
 
-          <Link to="/carrefours" className="dropdown-item">
-            Carrefours
-          </Link>
-          <Link to="/foodhalls" className="dropdown-item">
-            Foodhall
-          </Link>
-          <Link to="/farmers" className="dropdown-item">
-            Farmers
-          </Link>
+            <Link to="/carrefours" className="dropdown-item">
+              Carrefours
+            </Link>
+            <Link to="/foodhalls" className="dropdown-item">
+              Foodhall
+            </Link>
+            <Link to="/farmers" className="dropdown-item">
+              Farmers
+            </Link>
 
 
-        <Link to="/primos" className="dropdown-item">
-            Primo
-          </Link>
+            <Link to="/primos" className="dropdown-item">
+              Primo
+            </Link>
 
-        <Link to="/aeons" className="dropdown-item">
-            Aeon
-          </Link>
+            <Link to="/aeons" className="dropdown-item">
+              Aeon
+            </Link>
 
-        <Link to="/heros" className="dropdown-item">
-            Hero
-          </Link>
+            <Link to="/heros" className="dropdown-item">
+              Hero
+            </Link>
+          </div>
         </div>
-      </div>
-    </Fragment>
-  );
+      </Fragment>
+    );
 
-  return (
-    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <Link to="/" className="btn btn-dark">
-        Home
-      </Link>
-      {isAuthenticated ? normalLinks : ""}
-      {isAuthenticated ? authLinks : guestLinks}
-      <SocialIcon url="https://www.facebook.com/exportconnect/" target="_blank" bgColor="#3b5998" fgColor="white"/>
-      <SocialIcon url="https://www.instagram.com/exportconnect/" target="_blank" bgColor="#dd2a7b" fgColor="white"/>
-      <SocialIcon url="https://www.linkedin.com/company/exportconnect/" target="_blank" bgColor="#0e76a8" fgColor="white" />
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+          <Link to="/" className="btn btn-dark">
+            Home
+          </Link>
+          {isAuthenticated ? normalLinks : ""}
+          {isAuthenticated ? authLinks : guestLinks}
+        <div className="btn-group" role="group" aria-label="Third group">
+          <SocialIcon url="https://www.facebook.com/exportconnect/" target="_blank" bgColor="#3b5998" fgColor="white" style={{ height: 40, width: 40 }}/>
+          <SocialIcon url="https://www.instagram.com/exportconnect/" target="_blank" bgColor="#dd2a7b" fgColor="white" style={{ height: 40, width: 40 }}/>
+          <SocialIcon url="https://www.linkedin.com/company/exportconnect/" target="_blank" bgColor="#0e76a8" fgColor="white" style={{ height: 40, width: 40 }}/>
+        </div>
 
 
-    </nav>
-  );
-}
+      </nav>
+    );
+  }
 }
 const mapStateToProps = (state) => ({
   auth: state.auth,
