352e3204ecc28876782fa22e7068390dd0c1d14f
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-13 05:20:50 +1000

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style/Styles.css"
 import { MDBContainer,  MDBFooter } from "mdbreact";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const FooterPage = () => {
   return (
 
-    <MDBFooter className="footer">
+    <MDBFooter className="bg-dark text-white">
       <div className="footer-copyright text-center">
         <MDBContainer fluid>
           &copy; {new Date().getFullYear()} Developed by <a href="https://hasan-aliyev.site/"> HA</a>
