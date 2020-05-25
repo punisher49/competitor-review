702361f2c7dbf587f7c2149f9e35e0789c67802b
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-25 19:45:23 +1000

    google analitics

--- src/components/training/ExportTraining.js
+++ src/components/training/ExportTraining.js
@@ -6,50 +6,7 @@ class ExportTraining extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-6" id="videoDiv">
-              <h2>Why Export?</h2>
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe
-                  className="embed-responsive-item"
-                  src="//www.youtube.com/embed/ePbKGoIGAXY"
-                ></iframe>
-              </div>
-            </div>
-
-            <div className="col-sm-6" id="videoDiv">
-              <h2>Export vision & Kpi's</h2>
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe
-                  className="embed-responsive-item"
-                  src="//www.youtube.com/embed/ePbKGoIGAXY"
-                ></iframe>
-              </div>
-            </div>
-
-            <div className="col-sm-6" id="videoDiv">
-              <h2>Resourcing for export</h2>
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe
-                  className="embed-responsive-item"
-                  src="//www.youtube.com/embed/ePbKGoIGAXY"
-                ></iframe>
-              </div>
-            </div>
-
-            <div className="col-sm-6" id="videoDiv">
-              <h2>Market selection factors</h2>
-              <div className="embed-responsive embed-responsive-16by9">
-                <iframe
-                  className="embed-responsive-item"
-                  src="//www.youtube.com/embed/ePbKGoIGAXY"
-                ></iframe>
-              </div>
-            </div>
-          </div>
-        </div>
+      
       </div>
     );
   }
