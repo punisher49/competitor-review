9ca6f2561e7ee2e67c0ba0b19007202d0b718410
punisher49 <aliyev.hasanagha@gmail.com>
2020-05-27 14:52:50 +1000

    real videos

--- src/components/training/ExportTraining.js
+++ src/components/training/ExportTraining.js
@@ -5,35 +5,46 @@ import Navbar from "../Navbar";
 class ExportTraining extends Component {
   render() {
     return (
-      <div className="training">
+      <div className="bg-dark">
         <Navbar />
         <div className="container">
           <div className="row">
             <div className="col-sm-6" id="videoDiv">
-              <h3 className="headerColor">Why Export?</h3>
-              <div className="embed-responsive embed-responsive-4by3">
-                <iframe width={500} height={400} src="https://www.youtube.com/embed/p7HKvqRI_Bo" allowFullScreen="allowFullScreen" />
+
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe width={500} height={400} src="https://www.youtube.com/embed/cI196YhpQz4" allowFullScreen="allowFullScreen" />
+              </div>
+            </div>
+
+
+            <div className="col-sm-6" id="videoDiv">
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe width={500} height={400} src="https://www.youtube.com/embed/P1g5XFPBq8c" allowFullScreen="allowFullScreen" />
+              </div>
+            </div>
+
+
+
+            <div className="col-sm-6" id="videoDiv">
+
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe width={500} height={400} src="https://www.youtube.com/embed/kBUe51ekSHQ" allowFullScreen="allowFullScreen" />
               </div>
             </div>
 
             <div className="col-sm-6" id="videoDiv">
-              <h3 className="headerColor">Export vision & Kpi's</h3>
-              <div className="embed-responsive embed-responsive-4by3">
-                <iframe width={500} height={400} src="https://www.youtube.com/embed/p7HKvqRI_Bo" allowFullScreen="allowFullScreen" />
+
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe width={500} height={400} src="https://www.youtube.com/embed/x-Sr6RYX32M" allowFullScreen="allowFullScreen" />
               </div>
             </div>
 
-            <div className="col-sm-6" id="videoDiv">
-              <h3 className="headerColor">Resourcing for export</h3>
-              <div className="embed-responsive embed-responsive-4by3">
-                <iframe width={500} height={400} src="https://www.youtube.com/embed/p7HKvqRI_Bo" allowFullScreen="allowFullScreen" />
-              </div>
-            </div>
+
 
             <div className="col-sm-6" id="videoDiv">
-              <h3 className="headerColor">Market selection factors</h3>
-              <div className="embed-responsive embed-responsive-4by3">
-                <iframe width={500} height={400} src="https://www.youtube.com/embed/p7HKvqRI_Bo" allowFullScreen="allowFullScreen" />
+
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe width={500} height={400} src="https://www.youtube.com/embed/kTfySgKs3sc" allowFullScreen="allowFullScreen" />
               </div>
             </div>
           </div>
